ef10b8a6439a1cb4f698468c1a07c23bc9ab6dd5
Sean Beirnes <127370575+seanbeirnes@users.noreply.github.com>
2023-07-31 23:44:36 -0400

    Update levenshtein_calculator.py
    Added ability to run function without it being case sensitive.

--- python/levenshtein_calculator.py
+++ python/levenshtein_calculator.py
@@ -5,9 +5,11 @@
 # This version is preferred over the recurssive version due to its efficiency.
 # ###############################################
 
-def calc_cost(char_s, char_t):
+def calc_cost(char_s, char_t, check_case):
 
-    if char_s == char_t:
+    if check_case and char_s == char_t:
+        return 0
+    elif not check_case and char_s.lower() == char_t.lower():
         return 0
     else:
         return 1
@@ -23,14 +25,14 @@ def set_first_row(s):
     return row
 
 
-def add_row(s, t, last_row, int_t):
+def add_row(s, t, last_row, int_t, check_case):
 
     row = [ int_t + 1 ]
 
     for i in range( len(last_row[1:]) ):
         cell_above = last_row[ i + 1 ] + 1
         cell_left = row[i] + 1
-        cell_diag = last_row[i] + calc_cost(s[i], t[int_t])
+        cell_diag = last_row[i] + calc_cost(s[i], t[int_t], check_case)
 
         if cell_above < cell_left and cell_above < cell_diag:
             row.append( cell_above )
@@ -42,7 +44,7 @@ def add_row(s, t, last_row, int_t):
     return row
 
 
-def calc_lev(s, t):
+def calc_lev(s, t, check_case=True):
 
     matrix = []
 
@@ -51,12 +53,16 @@ def calc_lev(s, t):
     last_row = first_row
 
     for i in range( len(t) ):
-        new_row = add_row(s, t, last_row, i )
+        new_row = add_row(s, t, last_row, i , check_case)
         matrix.append(new_row)
         last_row = new_row
 
     return matrix[-1][-1]
 
+# Example implementation
+print(
+    calc_lev( input("First string: "), input("Second string: "), check_case=True )
+)
 
 # ###############################################
 # Recurssive Version
